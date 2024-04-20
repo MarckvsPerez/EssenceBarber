ab70e79d65433612507de5c304861eb2e7ab14c0
Marc Pérez Salat <marcantoniperez@gmail.com>
2024-04-20 03:50:14 +0200

    Fix: Loading component

--- src/components/LoadingComponent.tsx
+++ src/components/LoadingComponent.tsx
@@ -5,8 +5,8 @@ import {LogoEssence} from '../assets';
 export const LoadingComponent = () => {
 	return (
 		<div className='bg-black h-[100vh] w-full flex flex-col items-center justify-center text-white'>
-			<img className='w-[60vw]' src={LogoEssence} alt='Logo' />
-			<Box sx={{width: '60vw'}}>
+			<img className='w-[60vw] max-h-[410px] max-w-[500px]' src={LogoEssence} alt='Logo' />
+			<Box sx={{width: '60vw', maxWidth: ' 500px'}}>
 				<LinearProgress color='inherit' />
 			</Box>
 		</div>
